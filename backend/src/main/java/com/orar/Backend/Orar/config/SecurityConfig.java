a0f5d25f121ab96b480aaf54b943d1fa1e6f1d9d
includeSimon <33150228+includeSimon@users.noreply.github.com>
2025-06-07 18:21:21 +0300

    Update SecurityConfig.java

--- backend/src/main/java/com/orar/Backend/Orar/config/SecurityConfig.java
+++ backend/src/main/java/com/orar/Backend/Orar/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:*", "http://127.0.0.1:*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
