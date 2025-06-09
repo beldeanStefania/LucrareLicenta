9f081207ee2b73a0df03c3ae6a8f6f0b8930b748
Simon Pirlog <simon.pirlog@yahoo.com>
2025-06-09 11:43:06 +0300

    modified elastic ip

--- backend/src/main/java/com/orar/Backend/Orar/config/SecurityConfig.java
+++ backend/src/main/java/com/orar/Backend/Orar/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(
-                Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "http://54.155.59.152:30081"));
+                Arrays.asList("http://localhost:*", "http://127.0.0.1:*", "http://54.155.59.152:30080"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
