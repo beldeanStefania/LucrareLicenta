309fa9ab13a418ab1693966be05501cc72c4d616
Ștefania Beldean <stefaniabeldean@yahoo.com>
2025-06-10 22:34:48 +0300

    permit all

--- backend/src/main/java/com/orar/Backend/Orar/config/SecurityConfig.java
+++ backend/src/main/java/com/orar/Backend/Orar/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
                         }))
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        //http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
