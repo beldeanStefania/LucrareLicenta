8a51961abde66d741aa3d59b30dcb4b65044b2b7
Ștefania Beldean <stefaniabeldean@yahoo.com>
2025-06-27 13:33:12 +0300

    pipeline

--- frontend/src/setupTests.jsx
+++ frontend/src/setupTests.jsx
@@ -1,1 +1,12 @@
-import '@testing-library/jest-dom';
+// src/setupTests.ts
+import '@testing-library/jest-dom'
+import { vi } from 'vitest'
+
+// 1) mock pentru URL.createObjectURL
+Object.defineProperty(globalThis.URL, 'createObjectURL', {
+  writable: true,
+  value: vi.fn(),
+})
+
+// 2) mock pentru alert (dacă apare alerta în cod)
+globalThis.alert = vi.fn()
